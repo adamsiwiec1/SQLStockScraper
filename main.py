5cbc25000c426500c3b8bdfc8053318027713911
Adam Siwiec <acs0029@mix.wvu.edu>
2021-01-31 04:50:28 -0500

    finished basic implementation

--- main.py
+++ main.py
@@ -1,23 +1,76 @@
 # Using yahoo finance api
-import textwrap
-
 import yfinance as yf
+from stock import Stock
 
 
-def get_info():
-    nok = yf.Ticker('NOK')
-
-    # print("Info: " + str(nok.info) + "\n\n")
-    #
-    # print("Balance sheet: \n" + str(nok.balance_sheet) + "\n\n")
-    # print("Cashflow:  \n" + str(nok.cashflow) + "\n\n")
-    # print("Financials:  \n" + str(nok.financials) + "\n\n")
-    # print("Splits  \n" + str(nok.splits) + "\n\n")
-
-    print("Info: " + str(nok.info))
+# Mock Stocks
+stocksList = [Stock(str('NOK'), "", 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 1.00, 6.00)]
+stocksList.append(Stock(str('AZN'), "", 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 45.00, 50.00))
+stocksList.append(Stock(str('AAPL'), "", 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 99.00, 500.00))
+stocksList.append(Stock(str('TSLA'), "", 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 500.00, 900.00))
 
 
+def create_infoDict_list(stocks):
+    dictList = []
+    for stock in stocks:
+        if stock.volume is not None:
+            stokDict = get_info(str(stock.ticker))
+        try:
+            stock.ticker = stokDict["Ticker"]
+            stock.name = stokDict["Name"]
+            stock.price = stokDict["Price"]
+            stock.ask = stokDict["Ask"]
+            stock.bid = stokDict["Bid"]
+            stock.daylow = stokDict["DayLow"]
+            stock.dayhigh = stokDict["DayHigh"]
+            stock.volume = stokDict["Volume"]
+            stock.marketOpen = stokDict["MarketOpen"]
+            stock.marketClose = 'MarketClose'
+            dictList.append(stock.__dict__)
+        except UnboundLocalError as e:
+            print("Error" + str(e))
+    for dict in dictList:
+        print(dict)
 
-    # for i in ['Open', 'High', 'Close', 'Low']:
-    #     nok[i] = nok[i].astype('float64')
-get_info()
+
+def get_info(ticker):
+    stock = yf.Ticker(ticker)
+
+    infoDict = {
+
+        # current
+        "Ticker": stock.info['symbol'],
+        "Name": stock.info['longName'],
+        "Price": stock.info['regularMarketPrice'],
+        "Ask": stock.info['ask'],
+        "Bid": stock.info['bid'],
+        "DayLow": stock.info['dayLow'],
+        "DayHigh": stock.info['dayHigh'],
+        "Volume": stock.info['regularMarketVolume'],
+        "MarketOpen": stock.info['regularMarketOpen'],
+        "MarketClose": stock.info['regularMarketPreviousClose'],
+
+        # details
+        "52WeekLow": stock.info['fiftyTwoWeekLow'],
+        "52WeekHigh": stock.info['fiftyTwoWeekHigh'],
+        "50DayAvg": stock.info['fiftyDayAverage'],
+        "200DayAvg": stock.info['twoHundredDayAverage'],
+        "AvgVolume": stock.info['averageVolume'],
+        "10DayAvgVolume": stock.info['averageDailyVolume10Day'],
+
+        # extra details
+        "Sector": stock.info['sector'],
+        "BookValue": stock.info['bookValue'],
+        "YtdReturn": stock.info['ytdReturn'],
+        "LastDividendValue": stock.info['lastDividendValue'],
+        "ShareShort": stock.info['sharesShort'],
+        "FloatShares": stock.info['floatShares'],
+        "Employees": stock.info['fullTimeEmployees']
+
+
+    }
+
+    return infoDict
+
+
+create_infoDict_list(stocksList)
